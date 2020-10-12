bbdcf5d94435060ab0225790da0ce7e905c18ac0
iam-v0id <korampallysuhaas@gmail.com>
2020-10-12 16:49:15 +0530

    Updated DS

--- inv0k3r.py
+++ inv0k3r.py
@@ -3,7 +3,6 @@ from myimports import *
 
 print(banner.logo)
 
-Meeting_Link = {}        # Dictionary to store Meeting Links, with Timestamp as key
 Meeting_Times = []       # List to store keys(Timestamps) in sorted order
 
 
@@ -12,12 +11,12 @@ class Input(Thread):
         while True:
             Link = input('Enter Meeting Link : ')
             Time = list(map(int, input('Time (HH MM) : ').split()))
+            Duration = int(input('Duration of the meeting (Minutes) : '))
             curr = datetime.datetime.now()
             Meeting_Time = datetime.datetime(
                 curr.year, curr.month, curr.day, Time[0], Time[1]).timestamp()
 
-            Meeting_Link[Meeting_Time] = Link         # Storing Meeting Link
-            Meeting_Times.append(Meeting_Time)        # Keeping track of Key
+            Meeting_Times.append([Meeting_Time,Link,Duration])     
             Meeting_Times.sort()
 
 
@@ -34,19 +33,16 @@ if __name__ == "__main__":
         cmd = 'firefox'
 
     while True:
-        if len(Meeting_Times) > 0 and time.time() > Meeting_Times[0]:
+        if len(Meeting_Times) > 0 and time.time() > Meeting_Times[0][0]:
             # Kills the existing meeting if any
             if currPlatform == 'Windows':
                 os.system("TASKKILL /F  /IM  Zoom.exe > NUL 2>&1")
             elif currPlatform == 'Linux':
                 os.system("pkill zoom")
             # Starts the meeting
-            os.system('%s %s  > NUL 2>&1' %
-                      (cmd, Meeting_Link[Meeting_Times[0]]))
-            # Rejoins meeting after 40 mins
-            if 'zoom' in Meeting_Link[Meeting_Times[0]]:
-                time.sleep(40*60)
-                os.system('%s %s  > NUL 2>&1' %
-                          (cmd, Meeting_Link[Meeting_Times[0]]))
-            del Meeting_Link[Meeting_Times[0]]
-            Meeting_Times.pop(0)
+            for minutes in range(Meeting_Times[0][2]):            
+                os.system('%s %s  > NUL 2>&1' %(cmd, Meeting_Times[0][1]))
+                if 'zoom' not in Meeting_Times[0][1]:
+                    break
+                time.sleep(60)
+            del Meeting_Times[0]
